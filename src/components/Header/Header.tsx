2459bc1cbf8c23f02501fe47add54e95be341a9a
Alex <fess5800@mail.ru>
2026-03-29 20:19:19 +0300

    верстка header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,40 @@
+import { NavLink } from 'react-router-dom'
 import styles from './Header.module.css'
 
 const Header = () => {
     return (
         <header className={styles.header}>
             <div className={styles.container}>
-                <div className={styles.logo}>NeoBank</div>
+                <NavLink to='/' className={styles.logo}>
+                    NeoBank
+                </NavLink>
 
                 <nav className={styles.nav}>
-                <a href="#">Credit card</a>
-                <a href="#">About</a>
-                <a href="#">Contacts</a>
+                    <NavLink
+                        to="/credit-card"
+                        className={({ isActive }) =>
+                            isActive ? `${styles.link} ${styles.active}` : styles.link
+                        }
+                    >
+                        Credit card
+                    </NavLink>
+
+                    <NavLink to="/product" className={styles.link}>
+                        Product
+                    </NavLink>
+
+                    <NavLink to="/account" className={styles.link}>
+                        Account
+                    </NavLink>
+
+                    <NavLink to="/resources" className={styles.link}>
+                        Resources
+                    </NavLink>
                 </nav>
+
+                <button className={styles.button}>
+                    Online Bank
+                </button>
             </div>
         </header>
     )
